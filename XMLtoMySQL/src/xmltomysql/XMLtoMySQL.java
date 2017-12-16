bd8750e80253d4869f171d93e02a7a8877554abe
Isaac Rovira <rovira.isaac@gmail.com>
2017-12-16 06:03:57 +0100

    Joder joder

---
+++ XMLtoMySQL/src/xmltomysql/XMLtoMySQL.java
@@ -0,0 +1,251 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xmltomysql;
+import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import javax.xml.parsers.*;
+import javax.xml.parsers.*;
+import org.xml.sax.*;
+import org.w3c.dom.*;
+
+
+
+
+/**
+ *
+ * @author Isaac
+ */
+public class XMLtoMySQL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static class Album{
+
+        String album;
+        String albumArtist;
+        String composer;
+        String song;
+        String genero;
+        String año;
+        String pista;
+
+        public String getAlbum() {
+            return album;
+        }
+        public void setAlbum(String album) {
+            this.album = album;
+        }
+        public String getAlbumArtist() {
+            return albumArtist;
+        }
+        public void setAlbumArtist(String albumArtist) {
+            this.albumArtist = albumArtist;
+        }
+        public String getComposer() {
+            return composer;
+        }
+        public void setComposer(String composer) {
+            this.composer = composer;
+        }
+        public String getSong() {
+            return song;
+        }
+        public void setSong(String song) {
+            this.song = song;
+        }
+        public String getGenero() {
+            return genero;
+        }
+        public void setGenero(String genero) {
+            this.genero = genero;
+        }
+        public String getAño() {
+            return año;
+        }
+        public void setAño(String año) {
+            this.año = año;
+        }
+        public String getPista() {
+            return pista;
+        }
+        public void setPista(String pista) {
+            this.pista = pista;
+        }
+
+        public Album(String album, String albumArtist, String composer, String song, String genero, String año, String pista) {
+            this.album = album;
+            this.albumArtist = albumArtist;
+            this.composer = composer;
+            this.song = song;
+            this.genero = genero;
+            this.año = año;
+            this.pista = pista;
+        }
+        public Album(){
+            this.album="";
+            this.albumArtist="";
+            this.composer="";
+            this.song="";
+            this.genero="";
+            this.año="";
+            this.pista="";
+        }
+    }
+    public static class Songs{
+        String album;
+        String song;
+        String artist;
+        String pista;
+        String composer;
+
+        public String getComposer() {
+            return composer;
+        }
+
+        public void setComposer(String composer) {
+            this.composer = composer;
+        }
+        
+        public String getPista() {
+            return pista;
+        }
+
+        public void setPista(String pista) {
+            this.pista = pista;
+        }        
+        
+        public String getAlbum() {
+            return album;
+        }
+
+        public void setAlbum(String album) {
+            this.album = album;
+        }
+
+        public String getSong() {
+            return song;
+        }
+
+        public void setSong(String song) {
+            this.song = song;
+        }
+
+        public String getArtist() {
+            return artist;
+        }
+
+        public void setArtist(String artist) {
+            this.artist = artist;
+        }
+
+        public Songs() {
+        }
+
+        public Songs(String album, String song, String artist, String pista, String composer) {
+            this.album = album;
+            this.song = song;
+            this.artist = artist;
+            this.pista = pista;
+            this.composer = composer;
+        }        
+    }
+    
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+        for(int i = 0; i<15; i++){
+            System.out.println();            
+        }
+        ArrayList<Album> album = new ArrayList();
+        ArrayList<Songs> songs = new ArrayList();
+               
+        try{
+            File xmlFile = new File("h:\\tmp\\Biblioteca.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(xmlFile);
+            
+            doc.getDocumentElement().normalize();
+            
+            System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
+            
+            NodeList nList = doc.getElementsByTagName("key");
+            Album disco = new Album();
+            Songs cancion = new Songs();
+            boolean existe = false;
+            
+            for(int i=0; i<nList.getLength(); i++){
+                
+                //System.out.println(nList.item(i).getTextContent());
+                //System.out.println(nList.item(i).getTextContent()+" - "+ nList.item(i).getNextSibling().getTextContent()+"\n");
+                if(nList.item(i).getTextContent().equals("Track ID")){
+                    disco = new Album();
+                    cancion = new Songs();
+                    existe = false;
+                }
+                switch(nList.item(i).getTextContent()){
+                    case "Name": cancion.setSong(nList.item(i).getNextSibling().getTextContent());break;
+                    case "Artist": cancion.setArtist(nList.item(i).getNextSibling().getTextContent());break;
+                    case "Composer": cancion.setComposer(nList.item(i).getNextSibling().getTextContent());break;
+                    case "Album":
+                        cancion.setAlbum(nList.item(i).getNextSibling().getTextContent());
+                        for(Album a: album){
+                            if(a.getAlbum().equals(cancion.getAlbum())){
+                                existe = true;
+                                break;
+                            }
+                        }
+                        if(!existe){
+                            disco.setAlbum(cancion.getAlbum());
+                        }
+                        break;
+                    case "Genre":
+                        if(!existe){
+                        disco.setGenero(nList.item(i).getNextSibling().getTextContent());
+                        }
+                        break;
+                    case "Track Number":
+                        cancion.setPista(nList.item(i).getNextSibling().getTextContent());
+                        songs.add(cancion);
+                        break;
+                    case "Year": 
+                        if(!existe){
+                            disco.setAño(nList.item(i).getNextSibling().getTextContent());                                    
+                        }
+                        if(!existe){
+                            album.add(disco);
+                        }else{
+                            existe=true;
+                        }
+                        break;
+                    default:break;                                    
+                }
+            }            
+            /*
+            for(Songs valor: songs){
+                System.out.println(valor.getSong() + " - " + valor.getPista() + " - "+ valor.getAlbum());                        
+            }
+            */
+            for(Album valor: album){
+                System.out.println("INSERT INTO Discos (Album, Artista, año, Genero, SoporteID, Etiquetado, Identificadores, Discografica, Img_cover, Img_back) VALUES (\""+valor.getAlbum()+"\", \""+valor.albumArtist+"\", \""+valor.getAño()+"\", \""+valor.getGenero()+"\",1,\"\",\"\",\"\",\""+valor.getAlbum()+".jpg\",\"\");");
+            }
+            
+            System.out.println(album.size());
+            System.out.println(songs.size());
+            System.out.println(nList.getLength());
+                    
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        
+    }    
+}
---
+++ db/CREATE DB.sql
@@ -0,0 +1,102 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+/*BASE DE DATOS SOUNDLIB*/
+DROP DATABASE IF EXISTS soundlib;
+CREATE DATABASE soundlib DEFAULT CHARACTER SET latin1 COLLATE latin1_spanish_ci;
+USE soundlib;
+
+/*TABLAS*/
+/*tabla usuarios*/
+CREATE TABLE Users (
+    UserID int not null UNIQUE AUTO_INCREMENT,
+    Nombre varchar(255),
+    Apellidos varchar(255),
+    Alias varchar(255) not null UNIQUE,
+    Email varchar(55) not null UNIQUE,
+    Password varchar(255) not null,
+	ID_key varchar(25) not null UNIQUE,
+    PRIMARY KEY (UserID)
+	);
+
+/*SOPORTES*/
+CREATE TABLE Soportes(
+    SoporteID int not null UNIQUE AUTO_INCREMENT,
+    Tipo varchar(255) not null
+    );
+
+/*GENEROS*/
+CREATE TABLE Generos(
+	GeneroID int not null UNIQUE AUTO_INCREMENT,
+	GeneroNombre varchar(255) not null,
+	PRIMARY KEY (GeneroID)
+	);
+
+/*DISCOS*/
+CREATE TABLE Discos (
+    DiscoID int not null UNIQUE AUTO_INCREMENT,    
+    Album varchar(225) not null,
+    Artista varchar(255),	
+	Año int,
+    Genero varchar(255) not null,
+    SoporteID int not null,
+    Etiquetado varchar(255),
+    Identificadores varchar(255),
+	Discografica varchar(255),
+	Img_cover varchar(255),
+	Img_back varchar(255),
+    PRIMARY KEY (DiscoID)
+	);
+
+/*COVERS*/
+/*	
+CREATE TABLE Covers (
+	CoverID int not null UNIQUE AUTO_INCREMENT,
+	FileName varchar(255) not null,
+	Tipo enum ('Cover', 'Back') not null,
+	FullFileName varchar(255) not null UNIQUE,	
+	Tamaño enum ('S', 'M', 'L', 'XL') not null,	
+	PRIMARY KEY (CoverID)	
+	);	
+*/
+/*FONOTECAS*/
+CREATE TABLE Fonotecas(
+    FonoID int not null UNIQUE AUTO_INCREMENT,
+    Nombre varchar(255) not null,
+    UserID int not null,
+    PRIMARY KEY (FonoID)    
+    );
+	
+/*FONOTECASDATA*/
+CREATE TABLE FonotecasData(
+    FonotecasDataID int not null UNIQUE AUTO_INCREMENT,
+    DiscoID int not null,
+    FechaRegistro datetime not null,
+    NumItems int not null,
+	FonoID int not null,
+	PRIMARY KEY (FonotecasDataID),
+	FOREIGN KEY (FonoID) REFERENCES Fonotecas(FonoID)
+    );
+
+/*CANCIONES*/
+CREATE TABLE Canciones(
+	CancionID int not null UNIQUE AUTO_INCREMENT,
+    Titulo varchar(255) not null,
+    Pista int,
+    Duracion time,
+    Artistas TEXT,
+	DiscoID int,
+    PRIMARY KEY (CancionID)	
+    );
+	
+/*CONSTRAINTS*/
+/*
+ALTER TABLE Discos ADD FOREIGN KEY Chk_cover (Img_cover) REFERENCES Covers(CoverID);
+ALTER TABLE Discos ADD FOREIGN KEY Chk_back (Img_back) REFERENCES Covers(CoverID);
+*/
+ALTER TABLE Discos ADD FOREIGN KEY Chk_soporteId (SoporteID) REFERENCES Soportes(SoporteID);
+ALTER TABLE Fonotecas ADD FOREIGN KEY Chk_userId (UserID) REFERENCES Users(UserID);
+ALTER TABLE FonotecasData ADD FOREIGN KEY Chk_fonoId (FonoID) REFERENCES Fonotecas(FonoID);
+ALTER TABLE Canciones ADD FOREIGN KEY chk_discoId (DiscoID) REFERENCES Discos(DiscoID);
