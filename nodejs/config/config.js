84715a8174b9194c72f84d95a9585e2e1280ae51
IsaacRovira <rovira.isaac@gmail.com>
2018-01-01 02:32:25 +0100

    Commit todos los cambios hechos en nodejs en Oviedo

---
+++ nodejs/config/config.js
@@ -0,0 +1,12 @@
+//config.js
+
+const RAIZ      = process.cwd();
+const MODULOS   = RAIZ + "/node_modules/";
+const NODE      = "c:\\nodejs\\";
+
+
+module.exports ={
+    modulos     : MODULOS,
+    node        : NODE,
+    raiz        : RAIZ
+};
