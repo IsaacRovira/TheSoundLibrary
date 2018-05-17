83c877f1711f0b70b9468be174ba79e78a6303cd
Isaac Rovira <Isaac.Rovira@horibaabx.global>
2018-05-17 15:13:42 +0200

    Cambio en la configuración BD

--- config/config.js
+++ config/config.js
@@ -12,8 +12,8 @@ const APIPORT   = 3030;
 const HTTPPORT  = 80;
 
 //SQL
-const SQLPORT   = 3360;
-const SQLIP     = "127.0.0.1";
+const SQLPORT   = 3306;//3360
+const SQLIP     = "192.168.0.110"//"127.0.0.1";
 const SQLUSER   = "nodejs";
 const SQLPASS   = "node.js";
 const SQLDB     = "soundlib";
