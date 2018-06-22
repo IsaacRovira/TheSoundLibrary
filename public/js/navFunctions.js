6ac603d7a77a4a8734b0841365ded8ec6e59eede
Isaac Rovira <rovira.isaac@gmail.com>
2018-06-22 03:24:29 +0200

    Terminando funciones ordenar y buscar

--- public/js/navFunctions.js
+++ public/js/navFunctions.js
@@ -1,6 +1,6 @@
 //menuFunctions.js
 var openSubMenus = {
-    'search-form':     false,
+    'div-search-form':     false,
     'orderBy-form':   false
 };
 
@@ -30,44 +30,51 @@ function openNav() {
 ;
 //Refrescar contenido
  /* Volver a consultar la base de datos y acutlaizar el contenido mostrado según el modo seleccionado, lista o mosaico*/
-function refreshData(){
+function refreshData(){    
     doQueryAll(commonData.url.get()['std']['general']['discos'], commonData.datosDiscos.set);
     commonData.dataToSearch.set({});
     commonData.orderByField.set('discoId');
+    closeSubMenus();
     openNav();
 }
 ;
 //Buscar
-function openSearch(nodeId){    
-    var id = "search-form-selector";
+function openSearch(nodeId){
+    var id = "formSearch";
+    var pos = document.getElementById(nodeId).getBoundingClientRect();
+    var height = document.getElementById(id).offsetHeight;
+    
     switch(nodeId){
         case 'h-search':
-            var pos = document.getElementById(nodeId).getBoundingClientRect();            
+            closeIconOnOff('Buscar', nodeId);
+                if(height > 6){
+                    document.getElementById(id).style.height = "0";
+                    document.getElementById(nodeId).setAttribute('class', "menu-h-item col-1");
+                }else{        
+                    closeSubMenus();
+                    document.getElementById(nodeId).setAttribute('class', 'menu-h-item selected col-1');
+                    //Establecer la posición y el ancho respecto al menu BUSCAR.
+                    //document.getElementById(id).style.top = pos.bottom + "px";
+                    //document.getElementById(id).style.left = pos.left + "px";        
+                    //document.getElementById(id).style.width = "16vw";
+                    //Definir el alto para que el menú sea visible.
+                    //document.getElementById(id).style.height = "28vw";
+                    setPosnSize(pos.bottom, pos.left, 16, 28, id);
+                    openSubMenus[id] = true;
+                }
             break;
-        case 'v-search':            
+        case 'v-search':
+            if(true){}
             break;
     }
-    
-    height = document.getElementById(id).offsetHeight;
-    closeIconOnOff('Buscar', nodeId);
-    if(height > 0){        
-        document.getElementById(id).style.height = "0";
-        document.getElementById(nodeId).setAttribute('class', 'menu-h-item col-1');        
-    }else{        
-        //closeSubMenus();        
-        //Modificar la apariencia del menú buscar.
-        document.getElementById(nodeId).setAttribute('class', 'menu-h-item selected col-1');
-        
-        //Establecer la posición y el ancho respecto al menu BUSCAR.
-        document.getElementById(id).style.top = pos.bottom + "px";
-        document.getElementById(id).style.left = pos.left + "px";        
-        document.getElementById(id).style.width = pos.width + "px";
-        
-        //Definir el alto para que el menú sea visible.
-        document.getElementById(id).style.height = "6vw";
-        
-        openSubMenus[id] = true;
-    }        
+}
+;
+//Determina la posición altura y ancho de un objeto dado. Posición en px y tamaño en vw.
+function setPosnSize(t,l,w,h,id){
+    document.getElementById(id).style.top = t + "px";
+    document.getElementById(id).style.left = l + "px";
+    document.getElementById(id).style.width = w + "vw";
+    document.getElementById(id).style.height = h + "vw";
 }
 ;
 //Devuelve un nodo con los atributos pasados.
@@ -80,22 +87,23 @@ function createNode(tag, clase, id){
 ;
 //Remplaza en el elemento indicado el texto por el elemento <i class="fas fa-times"></i> y viceversa.
 function closeIconOnOff(text,id){   
-    var valor = document.getElementById(id).textContent;
+    var valor = document.getElementById(id).textContent;    
     switch(valor){
-        case text:
+        case text:            
             document.getElementById(id).textContent='';
-            var newNode = createNode('i','fas fa-times', 'h-sidenav-close-btn');//<i class="fas fa-times"></i>            
+            var newNode = createNode('i','fas fa-times', 'h-sidenav-close-btn');//<i class="fas fa-times"></i>
+            //newNode.setAttribute('onclick', openSearch(id));
             document.getElementById(id).insertBefore(newNode, null);            
             break;
-        default:
-            document.remove(document.getElementById('h-sidenav-close-btn'));
+        default:            
+            document.getElementById(id).removeChild(document.getElementById('h-sidenav-close-btn'));            
             document.getElementById(id).textContent = text;
     }
 }
 ;
 //busca discos q coincidan con los criterios.
 function searchAlbum(){
-    var elements = document.getElementById('formSearchAlbum').elements;
+    var elements = document.getElementById('formSearch').elements;
     var dataToSearch={};
     
     for(var key in elements){
@@ -123,15 +131,39 @@ function searchSong(){
 }
 ;
 //Ordenar
-function openOrderBy(){
-    var id = "orderBy-form";
-    width = document.getElementById(id).offsetWidth;
-    if(width > 0){
-        document.getElementById(id).style.width = "0";
-    }else{
-        closeSubMenus();
-        document.getElementById(id).style.width = "150px";
-        openSubMenus[id] = true;
+function openOrderBy(nodeId){    
+    closeSubMenus(); //Cerramos cualquier otro submenu abierto.
+    
+    switch(nodeId){ //Determinamos desde que menú han llamado a la función.
+        case 'h-orderby':
+            var id = "formFilter";
+            var heigh = document.getElementById(id);
+            if(heigh > 0){
+                document.getElementById(id).style.height = 0;
+                document.getElementById(id).style.width = 0;
+            }else{
+                //Determinamos la pos del objeto que lo ha llamado para posicionar nuestro menú justo debajo.
+                var pos = document.getElementById(nodeId).getBoundingClientRect();
+                //Modificamos la clase del elemento que ha llamado a la función.
+                document.getElementById(nodeId).setAttribute('class', 'menu-h-item selected col-1');
+                //Establecemos la posición y el ancho de nuestro submenu.
+                setPosnSize(pos.bottom, pos.left, 17, 18, id);
+                //Mostramos el elmento q cerrará el menú.
+                closeIconOnOff('Ordenar', nodeId);
+                openSubMenus[id] = true;                
+            }
+            break;
+        case 'v-orederby':
+            var id = "orderBy-form";
+            width = document.getElementById(id).offsetWidth;
+            if(width > 0){
+                document.getElementById(id).style.width = "0";
+            }else{
+                closeSubMenus();
+                document.getElementById(id).style.width = "150px";
+                openSubMenus[id] = true;
+            }
+            break;                        
     }
 }
 ;
@@ -161,6 +193,21 @@ function working(){
 }
 ;
 
+//ONSELECT SEARCH
+function cleanTextOnSelect(id){
+    var attrb = document.getElementById(id).getAttribute('value');
+    switch(attrb){
+        case 'Album':
+        case 'Artista':
+        case 'Año':
+        case 'Genero':
+        case 'Canción':
+            document.getElementById(id).setAttribute('value', '');
+        break;
+        default:            
+    }
+}
+;
 //Pictures error
 function getAltPic(id){
     document.getElementById(id).setAttribute('src', './img/Caratulas/nopic.jpg');
