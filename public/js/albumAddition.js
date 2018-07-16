4d041cc79ba69ecd8556b64b9baac3fba78cf070
Isaac Rovira <rovira.isaac@gmail.com>
2018-07-16 02:36:14 +0200

    Discogs api search - en construcción

---
+++ public/js/albumAddition.js
@@ -0,0 +1,148 @@
+/*
+ * albumAddtion.js
+ */
+
+/*DISCOGS API DATA*
+ * 
+ *https://api.discogs.com/database/search?q={back to black, Amy Winehouse, soul,release}&{?title,artist,genere,type}
+ *Header: Authorization: Discogs token=SdnGXnFwzcWVCMUhDmuumAZJOdCkpCcVGNApmdju
+ *
+ *--user-agent TheSoundLibrary/1.0 +http://thesoundlibrary.com
+ */
+
+var discogs={
+    url             : "https://api.discogs.com/database/search",
+    header          :{
+        header      :"authorization",
+        value       : "Discogs token=SdnGXnFwzcWVCMUhDmuumAZJOdCkpCcVGNApmdju"
+    },    
+    appinfo         :{
+        name        : "TheSoundLibrary/1.0 +http://thesoundlibrary.com",
+        type        : "user-agent"
+    },
+    pagination      :{
+        per_page    :25,
+        page        :1
+    }
+};
+
+
+
+
+/*
+ * Debe generar una cadena tipo: {back to black, Amy Winehouse, soul,release}&{?title,artist,genere,type}
+ * con los datos del formulario.
+ * Identificaremos que datos se han introducido y construiremos la cadena.
+ */
+function doSearch(){
+    //almacenamos el contenido del formulario
+    var elements = document.getElementById('searchFormAddition').elements;
+    //Estructura para almacenar los valores del formulario.
+    var dataToSearch={
+        artist          : "",
+        label           : "",
+        release_title   : "",
+        style           : "",
+        genre           : "",
+        format          : "",
+        country         : "",
+        barcode         : "",
+        year            : ""        
+    };
+    
+    //Cadenas de valores y parámetros.
+    var searchValues =""; //Valores a buscar    
+    
+    //Recorremos el objeto elements con los datos del formulario
+    for(var key in dataToSearch){
+        dataToSearch[key] = elements[key].value;
+    }
+    
+    for(var key in dataToSearch){
+        if(dataToSearch[key]){
+            if(searchValues.length > 0){
+                searchValues+="&" + key + '='+ dataToSearch[key];                
+            }else{
+                searchValues+=key + "=" + dataToSearch[key];                
+            }
+        }
+    }
+    
+    //paginación
+    var pagination ='&per_page='+discogs['pagination']['per_page']+'&page='+discogs['pagination']['page'];
+    
+    
+    alert(searchValues);
+    queryDiscogs(discogs['url'],searchValues, pagination, testResponse);
+};
+
+function queryDiscogs(destino, valores, param, callback) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {        
+        if (this.readyState === 4 && this.status === 200) {
+            callback(this.responseText);
+        }
+        if (this.status === 401){
+            alert("ACCESO NO AUTORIZADO");
+        }
+        //alert(this.status);
+    };
+    
+    //https://api.discogs.com/database/search?q={back to black, Amy Winehouse, soul,release}&{?title,artist,genere,type}
+    alert(destino+'?' + valores + param);
+    xhttp.open("GET", destino + '?' + valores + param, true);
+    xhttp.setRequestHeader(discogs['appinfo']['type'], discogs['appinfo']['name']);
+    xhttp.setRequestHeader(discogs['header']['header'], discogs['header']['value']);
+    xhttp.send();
+}
+;
+
+function testResponse(valor){    
+    data = JSON.parse(valor);  
+    var nodeUL = document.createElement('ul');
+    nodeUL.setAttribute('id','lista');
+    alert(data['pagination']['items']);    
+    for(var num = 0; num < data['results'].length; num ++){
+        var nodeLI = document.createElement('li');
+        for(var key in data['results'][num]){       
+            switch(key){
+                case 'style':
+                case 'format':
+                case 'label':
+                case 'barcode':
+                case 'genre':
+                    var nodeP = document.createElement('p');
+                    var nodeTXT = document.createTextNode(key + ": "+ JSON.stringify(data['results'][num][key]));
+                    nodeP.appendChild(nodeTXT);
+                    nodeLI.appendChild(nodeP);
+                    break;
+                case 'title':
+                    //Artista
+                    var nodeP = document.createElement('p');
+                    var nodeTXT = document.createTextNode('Artist' + ": "+ data['results'][num][key].split(' - ')[0]);
+                    nodeP.appendChild(nodeTXT);
+                    nodeLI.appendChild(nodeP);
+                    //Titulo
+                    var nodeP = document.createElement('p');
+                    var nodeTXT = document.createTextNode('Title' + ": "+ data['results'][num][key].split(' - ')[1]);
+                    nodeP.appendChild(nodeTXT);
+                    nodeLI.appendChild(nodeP);
+                    break;
+                case 'id':
+                case 'type':
+                case 'artist':
+                case 'year':            
+                case 'country':
+                    var nodeP = document.createElement('p');
+                    var nodeTXT = document.createTextNode(key + ": "+ data['results'][num][key]);
+                    nodeP.appendChild(nodeTXT);
+                    nodeLI.appendChild(nodeP);                    
+                    break;
+            }
+            nodeUL.appendChild(nodeLI);
+        }
+    }
+    //removeNode = document.getElementById('lista');
+    document.getElementById('lista').remove();
+    document.getElementById('mainContainer').appendChild(nodeUL);
+}
